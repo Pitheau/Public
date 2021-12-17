7447be6fce174b9981cd55a9dddc363632760c78
Pitheau <cpapitheau@gmail.com>
2021-12-17 13:30:10 -0800

    initial commit

---
+++ WEB-FUN_W1/Fizzbuzz/index.js
@@ -0,0 +1,15 @@
+for (let i = 1; i <= 100; i++) {
+
+    if (1 % 3 == 0 && i % 5 == 0) {
+        console.log( "FissBuzz")
+    }
+    else if (i % 3 == 0) {
+        console.log("Fizz");
+    }
+    else if (i % 5 == 0) {
+        console.log("Buzz");
+    }
+    else{
+        console.log(i);
+    }
+}
