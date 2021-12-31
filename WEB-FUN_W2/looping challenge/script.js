b0e5f1ea25b7b934a00a9256b59413fdb15b1ef9
Pitheau <cpapitheau@gmail.com>
2021-12-31 13:41:22 -0800

    revision

---
+++ WEB-FUN_W2/looping challenge/script.js
@@ -0,0 +1,19 @@
+// for(var i=1; i<20; i = i +2) {
+//     console.log(i);
+// }
+// for(var i=100; i >= 0; i -= 3) {
+//     console.log(i);
+// }
+// 
+// var sum = 0
+// for(var i=1; i<= 100; i++) {
+//     sum += i
+
+// }
+//     console.log(sufor(var i=1; i<20; i = i +2) {
+    var product = 1
+
+    for(var i=1; i<13; i++) {
+        product *= i;
+    }
+    console.log(product)
