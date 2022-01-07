9821ec1185009e0cb1466b56a35e8ee86ee2e709
Pitheau <cpapitheau@gmail.com>
2022-01-06 17:27:38 -0800

    reviewed

--- WEB-FUN_W2/array-challenges/script.js
+++ WEB-FUN_W2/array-challenges/script.js
@@ -9,7 +9,6 @@ function alwaysHungry(arr) {
         console.log("I'm hungry")
     }
 }
-   
 // alwaysHungry([3.14, "food", "pie", true, "food"]);
 // this should console log "yummy", "yummy"
 alwaysHungry([4, 1, 5, 7, 2]);
