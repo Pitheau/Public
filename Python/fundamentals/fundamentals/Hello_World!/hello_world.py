1d5ee65de860036aaa3f274441eb7398c187bce7
Pitheau <cpapitheau@gmail.com>
2022-01-09 15:55:13 -0800

    initial commit

---
+++ Python/fundamentals/fundamentals/Hello_World!/hello_world.py
@@ -0,0 +1,22 @@
+# 1. TASK: print "Hello World"
+print("Hello World")
+# 2. print "Hello Noelle!" with the name in a variable
+
+name = "Noelle"
+print("Hello World" , name)	# with a commax
+print("Hello World " + name)
+name = "42"
+print("Hello World " + name)
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print(f"I love to eat {fave_food1} and {fave_food2}.")
+
+"""
+print( your code here )	# with a +	-- this one should give us an error!
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1= "sushi"
+fave_food2= "pizza"
+print( your code here ) # with .format()
+print( your code here ) # with an f string
+
+"""
