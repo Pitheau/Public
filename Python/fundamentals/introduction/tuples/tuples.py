534f75f5b071aba034e904c160bbe5d1905a406d
Pitheau <cpapitheau@gmail.com>
2022-01-09 15:20:12 -0800

    initial commit

---
+++ Python/fundamentals/introduction/tuples/tuples.py
@@ -0,0 +1,11 @@
+tuple_data = ('physics', 'chemistry', 1997, 2000)
+tuple_num = (1, 2, 3, 4, 5 )
+tuple_letters = ("a", "b", "c", "d")
+print(tuple_data)
+print(tuple_num)
+print(tuple_letters)
+
+dog = ("Canis Familiaris", "dog", "carnivore", 12)
+print(dog[3])
+#result is: carnivore
+
