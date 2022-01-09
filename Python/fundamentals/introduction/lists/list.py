ecd567f45955bcb038d476c712c30b2aca5613f3
Pitheau <cpapitheau@gmail.com>
2022-01-09 14:31:19 -0800

    commit

--- Python/fundamentals/introduction/lists/list.py
+++ Python/fundamentals/introduction/lists/list.py
@@ -1,8 +1,15 @@
 drawer = ['documents', 'envelopes', 'pens']
+drawer.append('staples')
+
 #access the drawer with index of 0 and print value
 print(drawer[0])  #prints documents
 #access the drawer with index of 1 and print value
 print(drawer[1]) #prints envelopes
 #access the drawer with index of 2 and print value
 print(drawer[2]) #prints pens
-    
+print(drawer[3])
+
+x = [1,2,3,4,5]
+x.append(staples)
+print(x)
+#the output would be [1,2,3,4,5,99]
