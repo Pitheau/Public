561dc432a9b933f88b2c43d803bede4da5e7c885
Pitheau <cpapitheau@gmail.com>
2022-01-09 14:57:33 -0800

    commit

--- Python/fundamentals/introduction/lists/list.py
+++ Python/fundamentals/introduction/lists/list.py
@@ -1,15 +1,6 @@
-drawer = ['documents', 'envelopes', 'pens']
-drawer.append('staples')
 
-#access the drawer with index of 0 and print value
-print(drawer[0])  #prints documents
-#access the drawer with index of 1 and print value
-print(drawer[1]) #prints envelopes
-#access the drawer with index of 2 and print value
-print(drawer[2]) #prints pens
-print(drawer[3])
+print(my_list.index(4))
+tuple_data = ('physics', 'chemistry', 1997, 2000)
+tuple_num = (1, 2, 3, 4, 5 )
+tuple_letters = "a", "b", "c", "d"
 
-x = [1,2,3,4,5]
-x.append(staples)
-print(x)
-#the output would be [1,2,3,4,5,99]
