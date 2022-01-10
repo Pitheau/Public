d03dbde4b45000801c8a4b608f8476c8a4a32d13
Pitheau <cpapitheau@gmail.com>
2022-01-09 20:23:55 -0800

    initial commit

---
+++ Python/fundamentals/fundamentals/loops/loops.py
@@ -0,0 +1,10 @@
+y = 3
+while y > 0:
+    print(y)
+    y = y - 1
+    if y == 0:
+        break
+else:		# only executes on a clean exit from the while loop (i.e. not a break)
+   print("Final else statement")
+# output: 3, 2, 1
+
