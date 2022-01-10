bd85b80678496427372ec8b53dabff85e4791b59
Pitheau <cpapitheau@gmail.com>
2022-01-10 14:30:36 -0800

    commit

--- Python/fundamentals/fundamentals/functions/functions.py
+++ Python/fundamentals/fundamentals/functions/functions.py
@@ -1,11 +1,12 @@
 # invoking the function 3 times, passing in one argument each time
 
-
-
-def say_hi(name):
-    print("Hi, " + name)
-
-say_hi('Michael')
-say_hi('Anna')
-say_hi('Eli')
+def multiply(num_list, num):
+    for x in num_list:
+        x *= num
+    return num_list
+a = [2,4,10,16]
+b = multiply(a,5)
+print(b)
+# output:
+#[2,4,10,16]
 
