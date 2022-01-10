6d9a32d8f6a7dfadfad49620997918708b58d3db
Pitheau <cpapitheau@gmail.com>
2022-01-10 10:15:58 -0800

    initial commit

---
+++ Python/algos/W1D1.js
@@ -0,0 +1,46 @@
+/* 
+	Acronyms
+	Create a function that, given a string, returns the stringâ€™s acronym 
+	(first letter of each word capitalized). 
+	Do it with .split first if you need to, then try to do it without
+*/
+
+str1 = " there's no free lunch - gotta pay yer way. ";
+const expected1 = "TNFL-GPYW";
+
+const str2 = "Live from New York, it's Saturday Night!";
+const expected2 = "LFNYISN";
+arr = ()
+function acronym() {
+    arr = str1.split(" ")
+    console.log(arr)
+}
+	// .split() which will split up a string by a splitter
+	// var str = "Hello There"
+	// var arr = str.split(" ") -> ["Hello", "There"]
+	// console.log(str[0]) -> "H"
+	// console.log(arr[1][0]) -> "T"
+	// Loop through the string
+	// Grab the first character of each word
+	// add it to a new string
+	// capialize the new string
+// }
+
+/*****************************************************************************/
+
+/* 
+	String: Reverse
+	Given a string,
+	return a new string that is the given string reversed
+*/
+
+// const str1 = "creature";
+// const expected1 = "erutaerc";
+
+// const str2 = "dog";
+// const expected2 = "god";
+
+// function reverseString(str) {
+// 	// make a new string
+// 	// add to it the reversed letters
+// }
