5ed367f3106b9ed9f9e83f5967115040bf94339a
Pitheau <cpapitheau@gmail.com>
2022-01-10 20:36:03 -0800

    initial commit

--- python/fundamentals/function_basic2/function_basic2.py
+++ python/fundamentals/function_basic2/function_basic2.py
@@ -1,14 +1,28 @@
 # Countdown - Create a function that accepts a number as an input. Return a new list that counts down by one, from the number (as the 0th element) down to 0 (as the last element).
-# Example: countdown(5) should return [5,4,3,2,1,0]
-def countdown(x):
-    x in range(x, 0)
-    print(x)
 
-countdown(5)    
+# def countdown(x):
+#     for i in range(x, 0, -1):
+#         print(i)
 
-# Print and Return - Create a function that will receive a list with two numbers. Print the first value and return the second.
-# Example: print_and_return([1,2]) should print 1 and return 2
+# countdown(6)    
+
+# # Print and Return - Create a function that will receive a list with two numbers. Print the first value and return the second.
+# # Example: print_and_return([1,2]) should print 1 and return 2
+# def print_and_return(first, second):
+#     print(first)
+#     return(second)
+
+# print_and_return(12,40)
+
 # First Plus Length - Create a function that accepts a list and returns the sum of the first value in the list plus the list's length.
+def first_plus_length(x):
+    for i in x:
+        if i == 0:
+            print(i + (len(x))
+
+first_plus_length([20,19,18,17,16,15])
+
+
 # Example: first_plus_length([1,2,3,4,5]) should return 6 (first value: 1 + length: 5)
 # Values Greater than Second - Write a function that accepts a list and creates a new list containing only the values from the original list that are greater than its 2nd value. Print how many values this is and then return the new list. If the list has less than 2 elements, have the function return False
 # Example: values_greater_than_second([5,2,3,2,1,4]) should print 3 and return [5,3,4]
