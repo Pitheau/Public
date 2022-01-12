217c190ea83a678c88dd1a2917c35567f25b5d2e
Pitheau <cpapitheau@gmail.com>
2022-01-11 20:53:33 -0800

    updated

--- python/fundamentals/fundamentals/functions_intermediate_1/functions_intermediate_1.py
+++ python/fundamentals/fundamentals/functions_intermediate_1/functions_intermediate_1.py
@@ -1,26 +1,26 @@
-# x = [ [5,2,3], [10,8,9] ] 
-# x[1][0] = 15
-# print(x)
+x = [ [5,2,3], [10,8,9] ] 
+x[1][0] = 15
+print(x)
 
 
-# students = [
-#     {'first_name':  'Michael', 'last_name' : 'Jordan'},
-#     {'first_name' : 'John', 'last_name' : 'Rosales'}
-# ]
-# students[0]['last_name'] = 'Bryant'
-# print(students)
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+students[0]['last_name'] = 'Bryant'
+print(students)
 
-# sports_directory = {
-#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
-#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
-# }
-# sports_directory['soccer'][0] = 'Andres'
-# print(sports_directory)
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory)
 
-# z = [ {'x': 10, 'y': 20} ]
+z = [ {'x': 10, 'y': 20} ]
 
-# z[0]['y'] = '30'
-# print(z)
+z[0]['y'] = '30'
+print(z)
 
 
 students = [
