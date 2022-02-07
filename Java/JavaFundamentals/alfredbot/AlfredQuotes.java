a24886e30d478017e0f9299ed27a5bc0a1c08349
Pitheau <cpapitheau@gmail.com>
2022-02-07 14:57:22 -0800

    commit

---
+++ Java/JavaFundamentals/alfredbot/AlfredQuotes.java
@@ -0,0 +1,29 @@
+import java.util.Date;
+public class AlfredQuotes {
+    
+    public String basicGreeting() {
+        // You do not need to code here, this is an example method
+        return "Hello, lovely to see you. How are you?";
+    }
+    
+    public String guestGreeting(String name) {
+        // YOUR CODE HERE
+        return "place holder for guest greeting return string";
+    }
+    
+    public String dateAnnouncement() {
+        // YOUR CODE HERE
+        return "place holder for date announcement return string";
+    }
+    
+    public String respondBeforeAlexis(String conversation) {
+        // YOUR CODE HERE
+        return "for snarky response return string";
+    }
+    
+	// NINJA BONUS
+	// See the specs to overload the guessGreeting method
+    // SENSEI BONUS
+    // Write your own AlfredQuote method using any of the String methods you have learned!
+}
+
