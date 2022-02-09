9bf38d406f4942337a8d2615b6877a968abb6b7c
Pitheau <cpapitheau@gmail.com>
2022-02-08 20:04:40 -0800

    commit

--- Java/JavaFundamentals/CafeBusinessLogic/CafeUtil.java
+++ Java/JavaFundamentals/CafeBusinessLogic/CafeUtil.java
@@ -1,5 +1,10 @@
 public class CafeUtil {
     public static void main(String[] args) {
 
+    // public int getSteakGoal(){
+        
+    // }
+        
 
     }
+}
--- Java/JavaFundamentals/CafeBusinessLogic/TestCafe.java
+++ Java/JavaFundamentals/CafeBusinessLogic/TestCafe.java
@@ -2,17 +2,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 public class TestCafe {
     public static void main(String[] args) {
+      CafeUtil appTest = new CafeUtil();
         
     /* 
       You will need add 1 line to this file to create an instance 
       of the CafeUtil class. 
       Hint: it will need to correspond with the variable name used below..
     */
-    	
         /* ============ App Test Cases ============= */
     
         System.out.println("\n----- Streak Goal Test -----");
-        System.out.printf("Purchases needed by week 10: %s \n\n", appTest.getStreakGoal());
+        // System.out.printf("Purchases needed by week 10: %s \n\n", appTest.getStreakGoal());
     
         // System.out.println("----- Order Total Test-----");
         // double[] lineItems = {3.5, 1.5, 4.0, 4.5};
