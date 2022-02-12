d8bba7201c73f2ec93d212889cb10327a960e3ae
Pitheau <94864559+Pitheau@users.noreply.github.com>
2022-02-12 15:28:14 -0800

    Initial commit

---
+++ Java/hoppersReceipt/src/main/java/com/conradpitheau/hoppersReceipt/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.conradpitheau.hoppersReceipt;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HoppersReceiptApplication.class);
+	}
+
+}
