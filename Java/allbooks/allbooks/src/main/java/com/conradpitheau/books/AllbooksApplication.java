9306e534313b3891baed9a18dea68da8acd0bfa9
Pitheau <94864559+Pitheau@users.noreply.github.com>
2022-02-16 18:08:12 -0800

    Add files via upload

---
+++ Java/allbooks/allbooks/src/main/java/com/conradpitheau/books/AllbooksApplication.java
@@ -0,0 +1,13 @@
+package com.conradpitheau.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllbooksApplication.class, args);
+	}
+
+}
