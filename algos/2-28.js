479b09f3d6cff17440393b0f89fc916ce4b8cb49
Pitheau <cpapitheau@gmail.com>
2022-03-01 08:57:08 -0800

    algo work

--- algos/2-28.js
+++ algos/2-28.js
@@ -1,8 +1,105 @@
 /** reference: 
 http://btv.melezinek.cz/binary-search-tree.html
  */
+/** reference: 
+http://btv.melezinek.cz/binary-search-tree.html
+ */
 class Node {
-    constructor(data) {
+constructor(data) {
+this.data = data;
+this.left = null;
+this.right = null;
+}
+}
+
+
+class BinarySearchTree {
+constructor() {
+this.root = null;
+}
+
+isEmpty() {
+// determine if the BST is empty and return a boolean
+}
+
+min(current = this.root) {
+//return the smallest integer data
+if (current === null) {
+return null;
+}
+
+while (current.left) {
+current = current.left;
+}
+return current.data;
+}
+
+minRecursive(current = this.root) {
+if (current === null) {
+    return null;
+}
+
+if (current.left === null) {
+    return current.data;
+}
+return this.minRecursive(current.left);
+}
+
+max(current = this.root) {
+//return the largest integer data
+if (current === null) {
+return null;
+}
+
+while (current.right) {
+current = current.right;
+}
+return current.data;
+}
+
+maxRecursive(current = this.root) {
+if (current === null) {
+    return null;
+}
+
+if (current.right === null) {
+    return current.data;
+}
+return this.minRecursive(current.right);
+}
+//using recursive, finding the maximum
+}
+
+// Logs this tree horizontally with the root on the left.
+print(node = this.root, spaceCnt = 0, spaceIncr = 10) {
+    if (!node) {
+        return;
+    }
+}
+
+spaceCnt += spaceIncr;
+this.print(node.right, spaceCnt);
+
+console.log(
+    " ".repeat(spaceCnt < spaceIncr ? 0 : spaceCnt - spaceIncr) +
+    `${node.data}`
+);
+
+this.print(node.left, spaceCnt);
+}
+
+
+const emptyTree = new BinarySearchTree();
+
+
+const oneNodeTree = new BinarySearchTree();
+oneNodeTree.root = new Node(10);
+
+/** reference: 
+http://btv.melezinek.cz/binary-search-tree.html
+*/
+class Node {
+constructor(data) {
     this.data = data;
     this.left = null;
     this.right = null;
@@ -11,41 +108,64 @@ class Node {
 
 
 class BinarySearchTree {
-    constructor() {
-this.root = null;
-    }
+constructor() {
+    this.root = null;
+}
 
 isEmpty() {
-    // determine if the BST is empty and return a boolean
-    if(this.root = null){
-        return false;
-    }
-    return true;
+// determine if the BST is empty and return a boolean
 }
 
 min(current = this.root) {
-    //return the smallest integer data
-    if(this.root = null){
-        return false;
-    }
-    SVGGraphicsElement(current.left =)
+//return the smallest integer data
+if (current === null) {
+    return null;
+}
+
+while (current.left) {
+    current = current.left;
+}
+return current.data;
 }
 
 minRecursive(current = this.root) {
-    //using recursive, finding the minumum
-    //a method that called itself
+    if (current === null) {
+    return null;
+    }
+
+    if (current.left === null) {
+    return current.data;
+    }
+    return this.minRecursive(current.left);
 }
 
 max(current = this.root) {
-    //return the largest integer data
+//return the largest integer data
+if (current === null) {
+    return null;
+}
+
+while (current.right) {
+    current = current.right;
+}
+return current.data;
 }
 
 maxRecursive(current = this.root) {
-      //using recursive, finding the maximum
+    if (current === null) {
+    return null;
+    }
+
+    if (current.right === null) {
+    return current.data;
+    }
+    return this.minRecursive(current.right);
+}
+    //using recursive, finding the maximum
 }
 
-    // Logs this tree horizontally with the root on the left.
-    print(node = this.root, spaceCnt = 0, spaceIncr = 10) {
+// Logs this tree horizontally with the root on the left.
+print(node = this.root, spaceCnt = 0, spaceIncr = 10) {
     if (!node) {
     return;
     }
@@ -60,47 +180,10 @@ maxRecursive(current = this.root) {
 
     this.print(node.left, spaceCnt);
 }
-}
+
 
 const emptyTree = new BinarySearchTree();
 
 
 const oneNodeTree = new BinarySearchTree();
 oneNodeTree.root = new Node(10);
-
-
-
-
-/* twoLevelTree
-        root
-        10
-    /   \
-    5     15
-*/
-const twoLevelTree = new BinarySearchTree();
-twoLevelTree.root = new Node(10);
-twoLevelTree.root.left = new Node(5);
-twoLevelTree.root.right = new Node(15);
-twoLevelTree.print();
-
-
-/* threeLevelTree
-                    root
-                <-- 10 -->
-            /            \
-            5             15
-        /    \         /    \
-        2       8      12     20
-*/
-
-const threeLevelTree = new BinarySearchTree();
-threeLevelTree.root = new Node(10);
-threeLevelTree.root.left = new Node(5);
-threeLevelTree.root.right = new Node(15);
-threeLevelTree.root.left.left = new Node(2);
-threeLevelTree.root.left.right = new Node(8);
-threeLevelTree.root.right.left = new Node(12);
-threeLevelTree.root.right.right = new Node(20);
-threeLevelTree.print();
-
-console.log(isEmpty(oneNodeTree))
