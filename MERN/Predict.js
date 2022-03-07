637ed3b1d17064a0f94ed0ebb49ddc1964e3f4a6
Pitheau <cpapitheau@gmail.com>
2022-03-07 13:42:56 -0800

    commit

---
+++ MERN/Predict.js
@@ -0,0 +1,39 @@
+
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict displays Tesla then Mercedes
+console.log(randomCar)
+console.log(otherRandomCar)
+
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output Elon
+console.log(employee.name);
+console.log(otherName);
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output 12345 and undefined because password is not in person object
+console.log(password);
+console.log(hashedPassword);
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output false and true
+console.log(first == second);
+console.log(first == third);
+
