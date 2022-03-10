f85e0cf64b7c1d053364bdc37a0d330358c5ec0e
Pitheau <cpapitheau@gmail.com>
2022-03-10 09:45:46 -0800

    revision

--- algos/3-10.js
+++ algos/3-10.js
@@ -1,4 +1,9 @@
 
+const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const random = [-1, -5, 4, 10, 6, 22, 15]
 
 function insertionSort(nums) {
   //given an unsorted array, sort the array using insertSort
@@ -10,9 +15,20 @@ function insertionSort(nums) {
   // 5 is smaller than 9, so we move 5 to the correct position.
   // step4: [2, 5, 6, 9, 4]; ([2,5,6,9] is sorted) 
   // step5: [2, 4, 5, 6, 9]; ([2,4,5,6,9] is sorted) 
+for(let i = 1; i < nums.length; i++){
+  let currentInd = nums[i];
+  let x = i - 1
+  while((x > - 1) && (currentInd < nums[x])){
+      nums[x + 1] = nums[x];
+      x--
+  }
+  nums[x+1] = currentInd
+  }
+  return nums
 }
-
 console.log(insertionSort(numsRandomOrder))
+console.log(insertionSort(numsReversed))
+console.log(insertionSort(random))
 
 const arr1 = [2,4,8,3]
 const arr2 = [2,4,8,1]
@@ -23,4 +39,16 @@ const arr2 = [2,4,8,1]
 function insertionHelper(arr){
     // given an array with everything sorted except for the last element
   // insert the last element to the right position
+  for(let i = arr.length -1; i >= 0 ; i--){
+    let currentInd = arr[i];
+    let x = i - 1
+    while(currentInd < arr[x]){
+        arr[x + 1] = arr[x];
+        x--
+    }
+    arr[x+1] = currentInd
 }
+return arr
+}
+console.log(insertionHelper(arr1))
+console.log(insertionHelper(arr2))
