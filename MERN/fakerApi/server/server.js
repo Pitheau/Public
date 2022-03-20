b37b4cf0ae22fcb4c7b236982360ac515536678b
Pitheau <cpapitheau@gmail.com>
2022-03-19 17:59:38 -0700

    commit

--- MERN/fakerApi/server/server.js
+++ MERN/fakerApi/server/server.js
@@ -1,19 +1,50 @@
 const express = require("express");
+const { faker } = require('@faker-js/faker');
 const app = express();
 const port = 8000;
 
-
-
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello World you big old world" });
-});
-
-class Product {
+const users = []
+class Users {
     constructor() {
-        this.name = faker.commerce.productName();
-        this.price = "$" + faker.commerce.price();
-        this.department = faker.commerce.department();
-        }
+        this._id = faker.datatype.uuid()
+        this.firstName = faker.name.firstName();
+        this.lastName = faker.name.lastName();
+        this.phoneNumber =faker.phone.phoneNumber();
+        this.email = faker.internet.email();
+        this.password = faker.internet.password();
     }
+}
+class Company {
+    constructor() {
+        this._id = faker.datatype.uuid()
+        this.name = faker.commerce.productName();
+        this.street = faker.address.streetName();
+        this.city = faker.address.cityName();
+        this.state =faker.address.state();
+        this.zipCode =faker.address.zipCode();
+        this.country =faker.address.country();
+    }
+}
+
+//Routes
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
+
+
+//Post Create User
+app.post("/api/users/new", (req, res) =>{
+    const newUser = new Users()
+    users.push(newUser)
+    res.json(newUser)
+})
+app.post("/api/companies/new", (req, res) =>{
+    const newCompany = new Company()
+    users.push(newCompany)
+    res.json(newCompany)
+})
+
+console.log(new Users());
+console.log(new Company());
+
 
 app.listen( port, () => console.log(`Listening on port: ${port}`) );
